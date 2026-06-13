00a10a900079658e1a575951c79d280aed2f8472
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2026-06-13 09:50:49 +0100

    Fix macOS release build for 1.2.7 beta

--- fcs-gui/build.rs
+++ fcs-gui/build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    if std::env::var("CARGO_CFG_TARGET_OS").as_deref() != Ok("windows") {
+        return;
+    }
+
     #[cfg(windows)]
     {
         let mut res = winresource::WindowsResource::new();
